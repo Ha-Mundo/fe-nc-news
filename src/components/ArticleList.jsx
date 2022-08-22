690e2c8a90eaed09f2fe074f33953ae721a40402
Ha-Mundo <trauzzu@gmail.com>
2022-08-22 22:00:14 +0100

    added new material icons

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,8 +1,8 @@
 import { getArticles } from "../api";
 import React, { useState, useEffect } from "react";
 
-import CommentIcon from "@mui/icons-material/Comment";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import CommentTwoToneIcon from "@mui/icons-material/CommentTwoTone";
+import ThumbUpTwoToneIcon from "@mui/icons-material/ThumbUpTwoTone";
 
 const ArticleList = () => {
   const [articles, setArticles] = useState([]);
@@ -22,17 +22,26 @@ const ArticleList = () => {
             <div key={article.article_id} className="articleCard">
               <h4 className="articleTopic"> {article.topic} </h4>
               <h3 className="articleTitle"> {article.title}</h3>
-
-              <h5>Author: {article.author}</h5>
-              <h6>Date: {article.created_at.split("T")[0]} </h6>
+              <div className="articleDetails">
+                <h5>Author: {article.author}</h5>
+                <h6>Date: {article.created_at.split("T")[0]} </h6>
+              </div>
 
               <div className="article_card_footer">
                 <p>
-                  <ThumbUpIcon className="icon" color="primary" />{" "}
+                  <ThumbUpTwoToneIcon
+                    fontSize="large"
+                    className="icon"
+                    color="primary"
+                  />{" "}
                   {article.votes}
                 </p>
                 <p>
-                  <CommentIcon className="icon" color="primary" />{" "}
+                  <CommentTwoToneIcon
+                    fontSize="large"
+                    className="icon"
+                    color="primary"
+                  />{" "}
                   {article.comment_count}
                 </p>
               </div>
