9489368ad9e659f8caa27fe2b093a6c9070f8e44
Ha-Mundo <trauzzu@gmail.com>
2022-08-22 16:24:04 +0100

    Header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ArticleList from "./ArticleList";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Nc-News</h1>
+      <ArticleList />
+    </div>
+  );
+};
+
+export default Header;
