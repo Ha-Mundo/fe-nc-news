5307e0988ad8e8a9c20abe445129ae5f01970ce1
Ha-Mundo <trauzzu@gmail.com>
2022-08-23 23:43:54 +0100

    Home link added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>Nc-News</h1>
+      <Link to={"/"}>
+        <h1 id="title">Nc-News</h1>
+      </Link>
     </div>
   );
 };
