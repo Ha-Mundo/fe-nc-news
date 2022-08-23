caf326736564994db170bbfc311ff3647c294a95
Ha-Mundo <trauzzu@gmail.com>
2022-08-23 15:44:01 +0100

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import "./index.css";
-import React, { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Header />}></Route>
-          <Route path="/articles" element={<ArticleList />}></Route>
+          <Route path="/" element={<ArticleList />} />
+          <Route path="/articles/:topic" element={<ArticleList />} />
         </Routes>
       </BrowserRouter>
     </div>
