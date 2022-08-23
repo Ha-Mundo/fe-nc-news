7aea1ac813da2a72de00958a4201196193cc2562
Ha-Mundo <trauzzu@gmail.com>
2022-08-23 15:43:02 +0100

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-import ArticleList from "./ArticleList";
-
 const Header = () => {
   return (
     <div className="header">
       <h1>Nc-News</h1>
-      <ArticleList />
     </div>
   );
 };
