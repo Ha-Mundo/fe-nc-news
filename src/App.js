c97ba55fcb5a5a7bf05f0e44265666866c6f6cf8
Ha-Mundo <trauzzu@gmail.com>
2022-08-24 09:54:37 +0100

    app.js updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import ArticleList from "./components/ArticleList";
+import ArticleById from "./components/ArticleById";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<ArticleList />} />
-          <Route path="/articles/:topic" element={<ArticleList />} />
+          <Route path="/topics/:topic" element={<ArticleList />} />
+          <Route path="/articles/:article_id" element={<ArticleById />} />
         </Routes>
       </BrowserRouter>
     </div>
