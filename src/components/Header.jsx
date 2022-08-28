3db95c28566e45cf805a9e5548aa5bdb2f2d8911
Ha-Mundo <trauzzu@gmail.com>
2022-08-28 20:50:58 +0100

    current username added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../utils/Context";
 import Navbar from "./Navbar";
 
 const Header = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="header">
       <Link to={"/"}>
         <h1 id="title">Nc-News</h1>
       </Link>
       <Navbar />
+      <span>User: {user.username}</span>
     </div>
   );
 };
