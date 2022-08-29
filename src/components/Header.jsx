09a567235dbeb08f2360b458b5f24e243e71d0e5
Ha-Mundo <trauzzu@gmail.com>
2022-08-29 10:37:48 +0100

    user icon added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { UserContext } from "../utils/Context";
 import Navbar from "./Navbar";
 
+import { IconButton } from "@mui/material";
+import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+
 const Header = () => {
   const { user } = useContext(UserContext);
 
@@ -12,7 +15,16 @@ const Header = () => {
         <h1 id="title">Nc-News</h1>
       </Link>
       <Navbar />
-      <span id="user">User: {user.username}</span>
+      <span id="user">
+        <IconButton>
+          <AccountCircleTwoToneIcon
+            fontSize="large"
+            className="icon"
+            color="primary"
+          />
+        </IconButton>
+        {user.username}
+      </span>
     </div>
   );
 };
