1e1d39b8df73c43486f4b9c9b9062e569c2ffc72
Ha-Mundo <trauzzu@gmail.com>
2026-02-08 20:15:48 +0000

    refactor: simplify Header by extracting UserIcon component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,7 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../utils/Context";
 import Navbar from "./Navbar";
-
-import { IconButton } from "@mui/material";
-import AccountCircleTwoToneIcon from "@mui/icons-material/AccountCircleTwoTone";
+import UserIcon from "./UserIcon";
 
 const Header = () => {
   const { user } = useContext(UserContext);
@@ -14,23 +12,10 @@ const Header = () => {
       <Link to={"/"}>
         <h1 id="title">Nc-News</h1>
       </Link>
+
       <Navbar />
-      <span id="user">
-        <IconButton size="large">
-          <AccountCircleTwoToneIcon
-            sx={{
-              fontSize: {
-                xs: 32,
-                sm: 40,
-                md: 48,
-              },
-            }}
-            className="icon"
-            color="primary"
-          />
-        </IconButton>
-        {/* {user.username} */}
-      </span>
+
+      <UserIcon user={user} />
     </div>
   );
 };
