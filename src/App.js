39576f21792da2aa96aae181a7cc6fdfbbe074db
Ha-Mundo <trauzzu@gmail.com>
2026-02-09 19:38:32 +0000

    refactor(app): simplify App component and remove user logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./App.css";
-
-import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { getUsers } from "./utils/Api";
-import { UserContext } from "./utils/Context";
 
 import Header from "./components/Header";
 import ArticleList from "./components/ArticleList";
@@ -12,27 +7,19 @@ import ArticleById from "./components/ArticleById";
 import Footer from "./components/Footer";
 
 function App() {
-  const [user, setUser] = useState({ username: "tickle122" });
-
-  useEffect(() => {
-    getUsers().then(res => {
-      setUser(res[0]);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <UserContext.Provider value={{ user, setUser }}>
-        <BrowserRouter>
-          <Header />
+    <div className="app-container">
+      <BrowserRouter>
+        <Header />
+        <main className="main-content">
           <Routes>
             <Route path="/" element={<ArticleList />} />
             <Route path="/topics/:topic" element={<ArticleList />} />
             <Route path="/articles/:article_id" element={<ArticleById />} />
           </Routes>
-          <Footer />
-        </BrowserRouter>
-      </UserContext.Provider>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
