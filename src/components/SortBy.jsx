ce9c9978194e7903ed4d7999bbd0fa82a255ec1c
Ha-Mundo <trauzzu@gmail.com>
2026-02-10 11:41:03 +0000

    feat(sort): add comment_count option and synchronize select value

--- src/components/SortBy.jsx
+++ src/components/SortBy.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 const SortBy = ({ sortValue, setSortValue }) => {
-  const handleChange = event => {
+  const handleChange = (event) => {
     setSortValue(event.target.value);
   };
 
   return (
     <div>
       <label htmlFor="filter">Sort By: </label>
-      <select name="filter" id="filter" onChange={handleChange}>
+      <select 
+        name="filter" 
+        id="filter" 
+        value={sortValue} // this keep the UI sync
+        onChange={handleChange}
+      >
         <option value="created_at">Date</option>
-        <option value="votes">Most Popular</option>
         <option value="author">Author</option>
+        <option value="votes">Most Popular</option>
+        <option value="comment_count">Number of Comments</option>
       </select>
     </div>
   );
